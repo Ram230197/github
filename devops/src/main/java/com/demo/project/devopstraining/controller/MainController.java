852991e177f2c833c1a0a695e18b7400c4095185
Ramanathan J <rjmech97@gmail.com>
2021-11-18 11:41:50 +0530

    4th

--- devops/src/main/java/com/demo/project/devopstraining/controller/MainController.java
+++ devops/src/main/java/com/demo/project/devopstraining/controller/MainController.java
@@ -8,7 +8,7 @@ public class MainController {
 	
 	@GetMapping("/")
 	public String getmessage() {
-		return "Hi, Welcome to Devops Training 3rd check!!!";
+		return "Hi, Welcome to Devops Training 4th check!!!";
 	}
 
 }
